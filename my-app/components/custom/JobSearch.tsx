67bc3995ee647cb19fa95eb516c7ea7e33e4114b
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2025-01-16 12:59:23 -0600

    progress capture

--- my-app/components/custom/JobSearch.tsx
+++ my-app/components/custom/JobSearch.tsx
@@ -24,53 +24,58 @@ const JobSearch: React.FC = () => {
   const filteredJobs = filterJobs();
 
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>Job Search</h1>
-      {/* Department Filter */}
-      <div>
-        <label>
-          Department:
-          <select
-            value={selectedDepartment}
-            onChange={(e) => setSelectedDepartment(e.target.value)}
-          >
-            <option value="">All Departments</option>
-            {jobList.map((department) => (
-              <option key={department.department} value={department.department}>
-                {department.department}
-              </option>
-            ))}
-          </select>
-        </label>
-      </div>
+    <>
+      <div className="flex flex-row justify-between items-center border p-4">
+        {/* Search Filter */}
+        <div>{/* INPUT goes here */}</div>
+        {/* Department Filter */}
+        <div>
+          <label>
+            Department:
+            <select
+              value={selectedDepartment}
+              onChange={(e) => setSelectedDepartment(e.target.value)}
+            >
+              <option value="">All Departments</option>
+              {jobList.map((department) => (
+                <option
+                  key={department.department}
+                  value={department.department}
+                >
+                  {department.department}
+                </option>
+              ))}
+            </select>
+          </label>
+        </div>
 
-      {/* Location Filter */}
-      <div style={{ marginTop: "10px" }}>
-        <label>
-          Location:
-          <select
-            value={selectedLocation}
-            onChange={(e) => setSelectedLocation(e.target.value)}
-          >
-            <option value="">All Locations</option>
-            {/* Get unique list of cities */}
-            {Array.from(
-              new Set(
-                jobList.flatMap((department) =>
-                  department.jobs.flatMap((job) =>
-                    job.location.map((loc) => loc.city)
+        {/* Location Filter */}
+        <div style={{ marginTop: "10px" }}>
+          <label>
+            Location:
+            <select
+              value={selectedLocation}
+              onChange={(e) => setSelectedLocation(e.target.value)}
+            >
+              <option value="">All Locations</option>
+              {/* Get unique list of cities */}
+              {Array.from(
+                new Set(
+                  jobList.flatMap((department) =>
+                    department.jobs.flatMap((job) =>
+                      job.location.map((loc) => loc.city)
+                    )
                   )
                 )
-              )
-            ).map((city) => (
-              <option key={city} value={city}>
-                {city}
-              </option>
-            ))}
-          </select>
-        </label>
+              ).map((city) => (
+                <option key={city} value={city}>
+                  {city}
+                </option>
+              ))}
+            </select>
+          </label>
+        </div>
       </div>
-
       {/* Job Listing */}
       <div style={{ marginTop: "20px" }}>
         {filteredJobs.length ? (
@@ -91,7 +96,7 @@ const JobSearch: React.FC = () => {
           <p>No jobs found. Try adjusting your filters.</p>
         )}
       </div>
-    </div>
+    </>
   );
 };
 
